6d8625b8df6a15a2a218ed1a4c40a339109d5d31
Amitabh Ranjan <amitabh1ranjan@gmail.com>
2024-10-12 10:38:11 +0530

    Roman to Int code added as L4.java

--- L4.java
+++ L4.java
@@ -2,18 +2,31 @@ import java.util.*;
 class Solution {
     public int romanToInt(String s) {
 
+        int prevVal  = 0 ;
+        int total = 0;
+        
+
         for (int i = s.length() - 1; i >= 0; i--) {
+         
+            int value = romanToInt(s.charAt(i));   
             
-            int value = romanCharToInt(s.charAt(i));    
+            if (prevVal > value) {
+                
+                total = total - value;
+            } 
+            else{
+                total = total + value;
+            }
+           
         }
+        return total;
+
+    }
+        
 
 
-
-
-
-
-        public int romanToInt(char c) {
-        switch((c) ) {
+        private int romanToInt(char c) {
+        switch( c ) {
             case 'I' : return 1;
             case 'V' : return 5;
             case 'X' : return 10;
@@ -21,21 +34,22 @@ class Solution {
             case 'C' : return 100;
             case 'D' : return 500;
             case 'M' : return 1000;
+            default : return 0;
              }
-            }
-    }
+            
+    
 }
-
-public class Main {
+}
+public class L4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Roman value: ");
-        String s[] = new String[15];
-       
+        String s = sc.nextLine();
 
 
         Solution obj = new Solution();
-        int result = obj.romanToInt();
+        int result = obj.romanToInt(s);
+        System.out.println(result);;
         
     }
 }
